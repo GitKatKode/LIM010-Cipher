8e05f6a8ce1ebcba95d328e56143a3ed76b9cb88
Sab DevSet <sabrinaacm@gmail.com>
2019-06-03 12:32:55 -0500

    basic structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-/* Acá va tu código */
+const login = document.getElementById('section1');
+login.style.display = 'none';
+const tryAgain = document.getElementById('section2');
+tryAgain.style.display = 'none';
+const forbidden = document.getElementById('section3');
+forbidden.style.display = 'none';
