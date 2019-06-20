32162062926dc08817282e08b83e7048686b4c67
@KatKode <sabrinaacm@gmail.com>
2019-06-20 00:59:46 -0500

    cambio de cipher y actualización de test

--- src/cipher.js
+++ src/cipher.js
@@ -5,10 +5,35 @@ window.cipher = {
         for (let i = 0; i < string.length; i++) {
             const ASCII = string[i].charCodeAt();
             let encodedASCII;
-            if (ASCII >= 32 && ASCII <= 254) {
-                encodedASCII = (ASCII - 32 + offset) % 222 + 32;
+            if (ASCII >= 65 && ASCII <= 90) {
+                encodedASCII = (offset + ASCII - 65) % 26 + 65;
+                if (encodedASCII < 65) {
+                    encodedASCII = (offset + ASCII - 65) % 26 + 91;
+                }
+            } else if (ASCII >= 97 && ASCII <= 122) {
+                encodedASCII = (offset + ASCII - 97) % 26 + 97;
+                if (encodedASCII < 97) {
+                    encodedASCII = (offset + ASCII - 97) % 26 + 123;
+                }
+            } else if (ASCII >= 48 && ASCII <= 57) {
+                encodedASCII = (offset + ASCII - 48) % 10 + 48;
+                if (encodedASCII < 48) {
+                    encodedASCII = (offset + ASCII - 48) % 10 + 58;
+                }
+            } else if (ASCII >= 32 && ASCII <= 47) {
+                encodedASCII = (offset + ASCII - 32) % 16 + 32;
                 if (encodedASCII < 32) {
-                    encodedASCII = (ASCII - 32 + offset) % 222 + 254;
+                    encodedASCII = (offset + ASCII - 32) % 16 + 48;
+                }
+            } else if (ASCII >= 58 && ASCII <= 96) {
+                encodedASCII = (offset + ASCII - 58) % 39 + 58;
+                if (encodedASCII < 58) {
+                    encodedASCII = (offset + ASCII - 58) % 39 + 97;
+                }
+            } else if (ASCII >= 123 && ASCII <= 254) {
+                encodedASCII = (offset + ASCII - 123) % 132 + 123;
+                if (encodedASCII < 123) {
+                    encodedASCII = (offset + ASCII - 123) % 132 + 255;
                 }
             }
             encoded += String.fromCharCode(encodedASCII);
@@ -18,20 +43,47 @@ window.cipher = {
     decode: (offset, string) => {
         let decoded = '';
         offset = parseInt(offset);
-        for (let i = 0; i < string.length; i++) {
-            const ASCII = string[i].charCodeAt();
-            let decodedASCII;
-            if (ASCII >= 32 && ASCII <= 254) {
-                decodedASCII = (ASCII - offset % 222 - 32) + 32;
-                if (decodedASCII >= 254) {
-                    decodedASCII = (ASCII - offset % 222 - 254) + 32;
-                }
-            }
-            decoded += String.fromCharCode(decodedASCII);
-        }
+        decoded = cipher.encode(-offset, string);
         return decoded;
     },
 };
+
+
+// window.cipher = {
+//     encode: (offset, string) => {
+//         let encoded = '';
+//         offset = parseInt(offset);
+//         for (let i = 0; i < string.length; i++) {
+//             const ASCII = string[i].charCodeAt();
+//             let encodedASCII;
+//             if (ASCII >= 32 && ASCII <= 254) {
+//                 encodedASCII = (ASCII - 32 + offset) % 222 + 32;
+//                 if (encodedASCII < 32) {
+//                     encodedASCII = (ASCII - 32 + offset) % 222 + 254;
+//                 }
+//             }
+//             encoded += String.fromCharCode(encodedASCII);
+//         }
+//         return encoded;
+//     },
+//     decode: (offset, string) => {
+//         let decoded = '';
+//         offset = parseInt(offset);
+//         for (let i = 0; i < string.length; i++) {
+//             const ASCII = string[i].charCodeAt();
+//             let decodedASCII;
+//             if (ASCII >= 32 && ASCII <= 254) {
+//                 decodedASCII = (ASCII - offset % 222 - 32) + 32;
+//                 if (decodedASCII >= 254) {
+//                     decodedASCII = (ASCII - offset % 222 - 254) + 32;
+//                 }
+//             }
+//             decoded += String.fromCharCode(decodedASCII);
+//         }
+//         return decoded;
+//     },
+// };
+
 
 
 
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,12 +10,29 @@ describe('cipher', () => {
             assert.equal(typeof cipher.encode, 'function');
         });
 
-        it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 32', () => {
-            assert.equal(cipher.encode(32, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 'abcdefghijklmnopqrstuvwxyz');
+        it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
+            assert.equal(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
         });
-        // it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
-        //     assert.equal(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
-        // });
+
+        it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset 33', () => {
+            assert.equal(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz'), 'hijklmnopqrstuvwxyzabcdefg');
+        });
+
+        it('debería retornar "456" para "123" con offset 33', () => {
+            assert.equal(cipher.encode(33, '123'), '456');
+        });
+
+        it('debería retornar ")*+," para "()*+" con offset 33', () => {
+            assert.equal(cipher.encode(33, '()*+'), ')*+,');
+        });
+
+        it('debería retornar "]_`" para "<>?" con offset 33', () => {
+            assert.equal(cipher.decode(33, '<>?'), ']_`');
+        });
+
+        it('debería retornar "ò~ý" para "ÑáÜ" con offset 33', () => {
+            assert.equal(cipher.decode(33, 'ÑáÜ'), 'ò~ý');
+        });
     });
 
     describe('cipher.decode', () => {
@@ -24,12 +41,29 @@ describe('cipher', () => {
             assert.equal(typeof cipher.decode, 'function');
         });
 
-        it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "abcdefghijklmnopqrstuvwxyz" con offset 32', () => {
-            assert.equal(cipher.decode(32, 'abcdefghijklmnopqrstuvwxyz'), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
+            assert.equal(cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG'), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
         });
-        // it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
-        //     assert.equal(cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG'), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-        // });
+
+        it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "ijklmnopqrstuvwxyzabcdefg" con offset 33', () => {
+            assert.equal(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg'), 'abcdefghijklmnopqrstuvwxyz');
+        });
+
+        it('debería retornar "123" para "456" con offset 33', () => {
+            assert.equal(cipher.decode(33, '456'), '123');
+        });
+
+        it('debería retornar "()*+" para ")*+," con offset 33', () => {
+            assert.equal(cipher.decode(33, ')*+,'), '()*+)');
+        });
+
+        it('debería retornar "<>?" para "]_`" con offset 33', () => {
+            assert.equal(cipher.decode(33, ']_`'), '<>?');
+        });
+
+        it('debería retornar "ÑáÜ" para "ò~ý" con offset 33', () => {
+            assert.equal(cipher.decode(33, 'ò~ý'), 'ÑáÜ');
+        });
     });
 
 });
